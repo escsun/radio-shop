e3cca86e0c34b4ed01c9ce54db84bc5670765b9c
escsun <escsunarcher@gmail.com>
2016-11-19 05:39:26 +0200

    Исправлен файл миграции приложения accounts

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import django.contrib.auth.models
 import django.core.validators
+import django.contrib.auth.models
 import django.utils.timezone
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -17,32 +18,57 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
-                ('password', models.CharField(verbose_name='password', max_length=128)),
-                ('last_login', models.DateTimeField(verbose_name='last login', null=True, blank=True)),
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
                 ('is_superuser', models.BooleanField(verbose_name='superuser status', help_text='Designates that this user has all permissions without explicitly assigning them.', default=False)),
-                ('username', models.CharField(help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username. This value may contain only letters, numbers and @/./+/-/_ characters.', 'invalid')], unique=True, verbose_name='username', max_length=30, error_messages={'unique': 'A user with that username already exists.'})),
-                ('first_name', models.CharField(verbose_name='first name', max_length=30, blank=True)),
-                ('last_name', models.CharField(verbose_name='last name', max_length=30, blank=True)),
-                ('email', models.EmailField(verbose_name='email address', blank=True, max_length=254, unique=True)),
+                ('username', models.CharField(unique=True, help_text='Требуется, не больше 30 символов или меньше, латинские буквы, числа, знаки подчеркивания и дефис.', verbose_name='username', error_messages={'unique': 'A user with that username already exists.'}, max_length=30, validators=[django.core.validators.RegexValidator('^[A-z0-9_-]+$', 'Логин может содержать только латинские буквы, числа, знаки подчеркивания и дефис. Ограничения не больше 30 символов или меньше.', 'invalid')])),
+                ('first_name', models.CharField(blank=True, max_length=30, verbose_name='first name')),
+                ('last_name', models.CharField(blank=True, max_length=30, verbose_name='last name')),
+                ('email', models.EmailField(error_messages={'unique': 'Пользователь с таким электронным адресом уже существует.'}, unique=True, max_length=254, verbose_name='email address')),
                 ('is_staff', models.BooleanField(verbose_name='staff status', help_text='Designates whether the user can log into this admin site.', default=False)),
                 ('is_active', models.BooleanField(verbose_name='active', help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', default=True)),
                 ('date_joined', models.DateTimeField(verbose_name='date joined', default=django.utils.timezone.now)),
-                ('groups', models.ManyToManyField(help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', verbose_name='groups', related_query_name='user', to='auth.Group', blank=True)),
-                ('user_permissions', models.ManyToManyField(help_text='Specific permissions for this user.', related_name='user_set', verbose_name='user permissions', related_query_name='user', to='auth.Permission', blank=True)),
             ],
             options={
-                'swappable': 'AUTH_USER_MODEL',
-                'verbose_name': 'user',
                 'verbose_name_plural': 'users',
                 'abstract': False,
+                'verbose_name': 'user',
             },
             managers=[
                 ('objects', django.contrib.auth.models.UserManager()),
             ],
         ),
-        migrations.AlterUniqueTogether(
-            name='user',
-            unique_together=set([('email',)]),
+        migrations.CreateModel(
+            name='Profile',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True)),
+                ('activation_key', models.CharField(max_length=64)),
+                ('user', models.ForeignKey(related_name='profile', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+                'verbose_name_plural': 'Группы',
+                'verbose_name': 'Группа',
+            },
+            bases=('auth.group',),
+            managers=[
+                ('objects', django.contrib.auth.models.GroupManager()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='groups',
+            field=models.ManyToManyField(related_name='user_set', help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_query_name='user', verbose_name='groups', to='auth.Group', blank=True),
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='user_permissions',
+            field=models.ManyToManyField(related_name='user_set', help_text='Specific permissions for this user.', related_query_name='user', verbose_name='user permissions', to='auth.Permission', blank=True),
         ),
     ]
